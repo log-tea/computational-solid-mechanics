ee5c52d217f591a1ac8476a7cb4e96717a0f7b54
log-tea <logtea@outlook.com>
2024-11-01 21:29:22 +0800

    import nalgebra and test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod finite_element;
 
+extern crate nalgebra as na;
+use na::{ArrayStorage, Dynamic, Matrix, VecStorage, Vector3, U2, U3};
 use finite_element::lagrange::add;
 
 fn main() {
     println!("Hello, world!");
     add(1, 2);
+
+    let v = Vector3::new(1, 2, 3);
+    print!("{}",v)
     
 }
